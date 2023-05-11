d233e11c38632aabef812c03d4686e2cd3a40825
black-desk <me@black-desk.cn>
2023-05-11 14:42:51 +0800

    test(config): add test of invalid config
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 var _ = Describe("Configuration", func() {
-	ContextTable("load from valid configuration (%s)", func(path string) {
+	ContextTable("load from %s configuration (%s)", func(path string) {
 		var (
 			err     error
 			file    *os.File
@@ -40,6 +40,36 @@ var _ = Describe("Configuration", func() {
 	},
 		ContextTableEntry("../../test/data/example_config.yaml"),
 	)
+
+	ContextTable("load from invalid configuration (%s)", func(path string, expectErr error) {
+		var (
+			err     error
+			file    *os.File
+			content []byte
+		)
+
+		BeforeEach(func() {
+			if file, err = os.Open(path); err != nil {
+				Fail(fmt.Sprintf("Failed to open configuration %s: %s", path, err.Error()))
+			}
+
+			if content, err = io.ReadAll(file); err != nil {
+				Fail(fmt.Sprintf("Failed to read configuration %s: %s", path, err.Error()))
+			}
+
+			_, err = Load(content)
+		})
+		AfterEach(func() {
+			file.Close()
+		})
+		It(fmt.Sprintf("should fail with error: %s", expectErr), func() {
+			Expect(err).To(MatchError(expectErr))
+		})
+	},
+		ContextTableEntry("../../test/data/too_few_ports.yaml", ErrTooFewPorts).
+			WithFmt("../../test/data/too_few_ports.yaml"),
+	)
+
 })
 
 func TestConfig(t *testing.T) {
