448888eb6fc05fa75f821f1f7a436acd6f1a96c0
black-desk <me@black-desk.cn>
2023-05-12 17:02:36 +0800

    test(config): add tests
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,8 +8,11 @@ import (
 
 	. "github.com/black-desk/deepin-network-proxy-manager/internal/config"
 	. "github.com/black-desk/deepin-network-proxy-manager/internal/test/ginkgo-helper"
+	. "github.com/black-desk/deepin-network-proxy-manager/internal/test/gomega-helper"
+	"github.com/go-playground/validator/v10"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v3"
 )
 
 var _ = Describe("Configuration", func() {
@@ -62,12 +65,19 @@ var _ = Describe("Configuration", func() {
 		AfterEach(func() {
 			file.Close()
 		})
+
 		It(fmt.Sprintf("should fail with error: %s", expectErr), func() {
-			Expect(err).To(MatchError(expectErr))
+			Expect(err).To(MatchErr(expectErr))
 		})
 	},
 		ContextTableEntry("../../test/data/too_few_ports.yaml", ErrTooFewPorts).
 			WithFmt("../../test/data/too_few_ports.yaml"),
+		ContextTableEntry("../../test/data/wrong_type.yaml", new(yaml.TypeError)).
+			WithFmt("../../test/data/wrong_type.yaml"),
+		ContextTableEntry("../../test/data/validation_fail.yaml", validator.ValidationErrors{}).
+			WithFmt("../../test/data/validation_fail.yaml"),
+		ContextTableEntry("../../test/data/wrong_ports.yaml", new(ErrWrongPortsPattern)).
+			WithFmt("../../test/data/wrong_ports.yaml"),
 	)
 
 })
