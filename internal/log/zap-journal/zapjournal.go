9abe6ea73d22882b8c01539a242554af55054ec9
black-desk <me@black-desk.cn>
2023-05-18 16:56:03 +0800

    feat(zap-journal): add DebugConfig and NewDebug
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/log/zap-journal/zapjournal.go
+++ internal/log/zap-journal/zapjournal.go
@@ -29,6 +29,23 @@ func Config() zap.Config {
 	}
 }
 
+func DebugConfig() zap.Config {
+	return zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development: true,
+		Encoding:    "journal",
+		EncoderConfig: zapcore.EncoderConfig{
+			StacktraceKey:  "STACKTRACE",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.EpochTimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+		},
+		OutputPaths:      []string{"journal:///run/systemd/journal/socket"},
+		ErrorOutputPaths: []string{"journal:///run/systemd/journal/socket"},
+	}
+}
+
 func New(opts ...zap.Option) (ret *zap.Logger, err error) {
 	defer func() {
 		if err == nil {
@@ -42,6 +59,40 @@ func New(opts ...zap.Option) (ret *zap.Logger, err error) {
 
 	var log *zap.Logger
 	log, err = Config().Build()
+	if err != nil {
+		return
+	}
+
+	log = log.WithOptions(opts...)
+
+	var exe string
+	exe, err = os.Executable()
+	if err != nil {
+		err = fmt.Errorf(location.Capture()+
+			"Failed to get exe:\n%w", err,
+		)
+		return
+	}
+	log = log.Named(filepath.Base(exe))
+
+	ret = log
+
+	return
+}
+
+func NewDebug(opts ...zap.Option) (ret *zap.Logger, err error) {
+	defer func() {
+		if err == nil {
+			return
+		}
+		err = fmt.Errorf(location.Capture()+
+			"Failed to create new journald logger:\n%w",
+			err,
+		)
+	}()
+
+	var log *zap.Logger
+	log, err = DebugConfig().Build()
 	if err != nil {
 		return
 	}
