0270b4634d3074478f033aa9c6d23e904d3ae3e0
black-desk <me@black-desk.cn>
2023-05-19 00:06:13 +0800

    chore: remove unused consts
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -6,10 +6,8 @@ You might want check documentation and wiki on github for some help.
 https://github.com/black-desk/deepin-network-proxy-manager
 `
 
-	CfgPath                 = "/etc/deepin/network-proxy/config.yaml"
-	RepeaterMarkBase int32  = 3000
-	RepeaterMaxMarks uint32 = 20
-	RuleInsertHandle        = 1000
+	CfgPath          = "/etc/deepin/network-proxy/config.yaml"
+	RuleInsertHandle = 1000
 
 	PortsPattern = `^\[(\d+),(\d+)\)$`
 )
