9d3814d2772b29f330f6d3e6367acc03fbef6879
black-desk <me@black-desk.cn>
2023-06-22 00:03:10 +0800

    refact(log): use logger
    Signed-off-by: black-desk <me@black-desk.cn>

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,40 +1,12 @@
-//go:build !debug
-// +build !debug
-
 package logger
 
 import (
-	zapjournal "github.com/black-desk/zap-journal"
+	"github.com/black-desk/lib/go/logger"
 	"go.uber.org/zap"
-	"log"
 )
 
 var Log *zap.SugaredLogger
 
 func init() {
-	var (
-		logger *zap.Logger
-		err    error
-	)
-
-	defer func() {
-		Log = logger.Sugar()
-	}()
-
-	logger, err = zapjournal.New()
-	if err == nil {
-		return
-	}
-
-	log.Default().Printf("Failed to use zap-journal: %v", err)
-	log.Default().Printf("Fallback to zap default production logger.")
-
-	logger, err = zap.NewProduction()
-	if err == nil {
-		return
-	}
-
-	log.Default().Printf("Failed to use zap production logger: %v", err)
-	log.Default().Printf("Disable logging.")
-	logger = zap.NewNop()
+	Log = logger.Get("cgtproxy")
 }
