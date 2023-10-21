81a1f50b6470b107ffe2fb1ac239f69cc409304e
black-desk <me@black-desk.cn>
2023-10-21 23:29:11 +0800

    chore: update install script
    Signed-off-by: black-desk <me@black-desk.cn>

--- docs/install.md
+++ docs/install.md
@@ -7,5 +7,5 @@
 2. use the shell script
 
    ```bash
-   curl -fL https://raw.githubusercontent.com/black-desk/cgtproxy/master/scripts/get_cgtproxy.sh | sh
+   curl -fL https://raw.githubusercontent.com/black-desk/cgtproxy/master/scripts/get.sh | sh
    ```
---
+++ scripts/get.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# shellcheck disable=SC1090
+. <(curl -fL "https://raw.githubusercontent.com/black-desk/get/master/get.sh") \
+	black-desk cgtproxy
+
+curl -fLO "https://raw.githubusercontent.com/black-desk/cgtproxy/v$VERSION/misc/systemd/cgtproxy.service"
+
+$SUDO install -m755 -D "$TMP_DIR/cgtproxy" "$PREFIX/bin/cgtproxy"
+$SUDO install -m644 -D "$TMP_DIR/cgtproxy.service" "$PREFIX/lib/systemd/system/cgtproxy.service"
